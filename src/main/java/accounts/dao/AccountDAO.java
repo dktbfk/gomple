8e98f3e2f010f92883d8e8c1901c5576ebb01dce
Administrator <Administrator@MSDN-SPECIAL>
2014-03-29 17:21:54 +0900

    ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ

---
+++ src/main/java/accounts/dao/AccountDAO.java
@@ -0,0 +1,60 @@
+package accounts.dao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+
+import accounts.dto.AccountDTO;
+import accounts.util.AccountDBUtil;
+
+public class AccountDAO {
+	
+	public static List selectAllAccounts() throws SQLException{
+		SqlSession session = AccountDBUtil.getSqlSession();
+		List selectAll = null;
+		
+		try{
+			selectAll = session.selectList("Account.selectAllAccounts");
+		}finally{
+			session.close();
+		}
+		return selectAll;
+	}
+	
+	public static AccountDTO selectAccountById(String id, int pw) throws SQLException{
+		SqlSession session = AccountDBUtil.getSqlSession();
+		AccountDTO account = null;
+		try {
+			account = (AccountDTO)session.selectOne("selectAccountById",id);
+		} finally {
+			session.close();
+		}
+		
+		return account;
+	}
+	
+	public static void insertAcccount(AccountDTO account) throws SQLException{
+		SqlSession session = AccountDBUtil.getSqlSession(true);
+		try{
+			session.insert("insertAccount", account);
+		}finally{
+			session.close();
+		}
+		
+	}
+	
+	public static void deleteAccount(String id) throws SQLException{
+		SqlSession session = AccountDBUtil.getSqlSession(true);
+		try{
+			session.delete("deleteAccount", id);
+		}finally{
+			session.close();
+		}
+		
+	}
+	
+	
+	
+
+}
